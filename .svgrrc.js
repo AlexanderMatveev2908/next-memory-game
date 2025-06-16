e4f2cb305416c6c95f4e7ad977aebe10ca426300
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-16 12:59:19 +0300

    made config svgr/cli for SVGs

---
+++ .svgrrc.js
@@ -0,0 +1,17 @@
+/** @type {import('@svgr/core').Config} */
+module.exports = {
+  typescript: true,
+  icon: true,
+  svgProps: {
+    "aria-hidden": "true",
+    fill: "currentColor",
+  },
+  prettier: true,
+  expandProps: "end",
+  replaceAttrValues: {
+    "#000": "currentColor",
+    "#000000": "currentColor",
+    black: "currentColor",
+    currentColor: "inherit",
+  },
+};
--- src/app/some-page/page.tsx
+++ src/app/some-page/page.tsx
@@ -1,10 +1,18 @@
 "use client";
+import { GamePad } from "@/shared/SVGs";
 import type { FC } from "react";
 
 const Page: FC = () => {
   console.log(process.env.NEXT_PUBLIC_BANANAS_SUPPLY__CLIENT);
 
-  return <div className="text-red-600">some page</div>;
+  return (
+    <div className="">
+      <GamePad
+        className="w-[100px] h-[100px]"
+        {...{ fill: "red", stroke: "blue" }}
+      />
+    </div>
+  );
 };
 
 export default Page;
