edcd9e91b0e6aefef1115b46d3851623cd9935b9
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-17 15:55:43 +0300

    made component for texts

--- src/app/components/Home/OptUserChoice.tsx
+++ src/app/components/Home/OptUserChoice.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import type { FC } from "react";
-import OptUserChoiceHeader from "./fragments/Header/OptUserChoiceHeader";
 import { OptUserChoiceStyled } from "./Styled";
 import OptUserChoiceForm from "./fragments/Form/OptUserChoiceForm";
 import { useSelector } from "react-redux";
 import { getUserChoiceState } from "../../../features/optUserChoice/slice";
+import Txt from "@/shared/components/Txt/Txt";
 
 const OptUserChoice: FC = () => {
   const state = useSelector(getUserChoiceState);
@@ -13,8 +13,10 @@ const OptUserChoice: FC = () => {
   console.log(state);
 
   return (
-    <OptUserChoiceStyled className="w-full grid grid-cols-1 max-w-fit">
-      <OptUserChoiceHeader />
+    <OptUserChoiceStyled className="w-full grid grid-cols-1">
+      <div className="w-full flex justify-center h-fit">
+        <Txt {...{ txt: "memory", $fsz: "var(--h__xl)" }} />
+      </div>
 
       <OptUserChoiceForm />
     </OptUserChoiceStyled>
---
+++ src/shared/components/Txt/Styled.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { resp } from "@/core/lib/style";
+import styled from "styled-components";
+import { PropsTypeTxt } from "./Txt";
+
+export const TxtStyled = styled.div<Omit<PropsTypeTxt, "txt">>`
+  color: ${({ $clr }) => $clr ?? "var(--white_1)"};
+  font-size: ${({ $fsz }) => $fsz ?? "var(--h__lg)"};
+  font-weight: 700;
+
+  ${resp("md")} {
+    font-size: ${({ $fsz_md }) => $fsz_md ?? "var(--h__xl)"};
+  }
+
+  ${resp("lg")} {
+    font-size: ${({ $fsz_lg }) => $fsz_lg ?? ""};
+  }
+`;
