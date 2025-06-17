143849db8f9f91f2a9036cd731fae439e2c1f18d
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-17 08:43:34 +0300

    will create color classes for texts, taiwind config still not apply theme

---
+++ src/features/optUserChoice/components/fragments/OptUserChoiceHeader.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+const OptUserChoiceHeader: FC = () => {
+  return (
+    <div className="w-full flex justify-center">
+      <span className="txt_h__xl txt_clr__white_1">memory</span>
+    </div>
+  );
+};
+
+export default OptUserChoiceHeader;
