5d88db5c9afee8d36a3bc2a2f6b29c0a4105efe2
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-17 14:00:52 +0300

    made a recrive utils IDs start try using it

--- src/core/lib/style/index.ts
+++ src/core/lib/style/index.ts
@@ -3,3 +3,5 @@ import { BreakType } from "@/shared/types/style";
 
 export const resp = (brk: BreakType): string =>
   `@media screen and (min-width: ${breaks[brk]}px)`;
+
+export const decap = (str: string) => str.at(0) + str.slice(1);
