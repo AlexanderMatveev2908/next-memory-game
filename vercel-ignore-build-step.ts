8ec8bba4943e92f9f6f29185334a877ad80b4461
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-17 09:20:03 +0300

    update vercel.json

---
+++ vercel-ignore-build-step.ts
@@ -0,0 +1,11 @@
+import { env, exit } from "process";
+
+const branch = env.VERCEL_GIT_COMMIT_REF;
+
+if (branch !== "main") {
+  console.log(`🛑 Skipping build for branch: ${branch}`);
+  exit(0);
+}
+
+console.log(`✅ Proceeding with build for branch: ${branch}`);
+exit(1);
