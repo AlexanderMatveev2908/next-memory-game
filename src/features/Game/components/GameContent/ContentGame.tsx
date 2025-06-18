88ebd6ed33a93bce86739cfcb9dea2db86715227
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-18 14:09:51 +0300

    fix errors names

---
+++ src/features/Game/components/GameContent/ContentGame.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import type { FC } from "react";
+import { GameContentStyled } from "./Styled";
+
+type PropsType = {};
+
+const GameContent: FC<PropsType> = ({}) => {
+  return <GameContentStyled></GameContentStyled>;
+};
+
+export default GameContent;
---
+++ src/features/Game/components/GameFooter/GameFooter.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import type { FC } from 'react';
+import { GameFooterStyled } from './Styled';
+
+type PropsType = {
+  
+};
+
+const GameFooter: FC<PropsType> = ({ }) => {
+  return (
+    <GameFooterStyled>
+	
+    </GameFooterStyled>
+  );
+};
+
+export default GameFooter;
