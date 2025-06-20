d0ec78460eccdaaece4550d1ffe858172904d080
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-06-20 09:45:56 +0300

    try deply to see how appear

---
+++ src/app/components/Loading/Spinner.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import type { FC } from "react";
+import { SpinnerStyled } from "./Styled";
+
+const Spinner: FC = () => {
+  return <SpinnerStyled></SpinnerStyled>;
+};
+
+export default Spinner;
